74276a91d21d614104e851b0205e33264c301d20
YangciciWang <39508866+YangciciWang@users.noreply.github.com>
2018-06-07 08:58:30 +0800

    test your function of GPU

--- test002.py
+++ test002.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-a = tf.constant(67)
-b = tf.constant(454)
-sess = tf.Session()
-print(sess.run(a+b))
+a = tf.constant([1, 2, 3], shape=[3], name='a')
+b = tf.constant([4, 5, 6], shape=[3], name='b')
+c = a + b
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+print(sess.run(c))
 
 # tf.__version__
