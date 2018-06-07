f1c3e53e3140e88c65003b14e6f29056c0fabe8f
YangciciWang <39508866+YangciciWang@users.noreply.github.com>
2018-06-07 16:36:02 +0800

    Add files via upload

--- test002.py
+++ test002.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import os
+import timeit
+start = timeit.default_timer()
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 a = tf.constant([1, 2, 3], shape=[3], name='a')
@@ -7,5 +9,7 @@ b = tf.constant([4, 5, 6], shape=[3], name='b')
 c = a + b
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 print(sess.run(c))
+end = timeit.default_timer()
+print("运行时间：%.4f s" % (end - start))
 
 # tf.__version__
